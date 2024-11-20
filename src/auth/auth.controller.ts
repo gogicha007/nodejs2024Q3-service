d6172b1e63256c273effbd061d565c4a18b28bfc
gogicha007 <gogicha@gmail.com>
2024-11-20 17:51:53 +0400

    chore: save the progress

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,27 @@
-import { Body, Controller, Post, HttpCode } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService){}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('signup')
-    @HttpCode(201)
-    async signup(@Body() createUserDto: CreateUserDto){
-        return await this.authService.signup(createUserDto)
-    }
+  @Post('signup')
+  @HttpCode(201)
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.signup(createUserDto);
+  }
 
-    
+  @Post('login')
+  @HttpCode(200)
+  async login(@Req() req: Request) {
+    return await this.authService.login(req.body);
+  }
 
+  @Post('refresh')
+  @HttpCode(200)
+  async refresh(@Req() req: Request) {
+    return await this.authService.refresh(req.body);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,42 @@
 import { Injectable } from '@nestjs/common';
-import { from, Observable } from 'rxjs';
+import { Request } from 'express';
+import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UserService } from 'src/users/users.service';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly userService: UserService,
-  ) {}
+  constructor(private readonly dbService: DatabaseService,private readonly userService: UserService) {}
 
   async signup(createUserDto: CreateUserDto) {
-    createUserDto.password = await this.hashPassword(createUserDto.password)
+    
+    createUserDto.password = await this.hashPassword(createUserDto.password);
     return await this.userService.create(createUserDto);
   }
 
-  async hashPassword(password: string) {
-    const salt = await bcrypt.genSalt(+process.env.CRYPT_SALT)
-    const hashedPass = await bcrypt.hash(password, salt);
-    return hashedPass
+  async login(body: { login: string; password: string }) {
+    console.log(body.login);
+    const userExist = await this.dbService.user.findUnique({
+        where: {
+            login: body.login
+        }
+    })
+    if(userExist) console.log(userExist)
+    return body;
   }
 
-  comparePasswords(password: string, oldPasswordHash: string): Observable<any> {
-    return from(bcrypt.compare(password, oldPasswordHash));
+  async refresh(request) {}
+
+  async hashPassword(password: string) {
+    const salt = await bcrypt.genSalt(+process.env.CRYPT_SALT);
+    const hashedPass = await bcrypt.hash(password, salt);
+    return hashedPass;
+  }
+
+  async comparePasswords(password: string, storedPasswordHash: string) {
+    const result = await bcrypt.compare(password, storedPasswordHash);
+    return result;
   }
 }
